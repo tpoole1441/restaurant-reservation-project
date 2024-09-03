697e587b83dcc0cbb053d78f18ee167f71f53a61
Tyler Poole <tyler.poole1441@gmail.com>
2024-09-02 20:36:51 -0500

    updated list functionality

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,11 +1,12 @@
-const { useParams } = require("react");
+const reservationsService = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const date = req.query.date;
+  const data = await reservationsService.list(date);
+  return res.json({ data });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,3 +1,12 @@
 const knex = require("../db/connection");
 
-function list() {}
+function list(reservation_date) {
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_date })
+    .orderBy("reservation_time");
+}
+
+module.exports = {
+  list,
+};
